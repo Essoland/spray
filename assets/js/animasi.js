2bd9f0c75184045122a6e6417a00aab5c5ae84f7
prasetyoedi <prasetyoedi451@gmail.com>
2025-08-05 20:10:16 +0700

    up web essoland

---
+++ assets/js/animasi.js
@@ -0,0 +1,116 @@
+var textWrapper = document.querySelector(".header-1");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+    targets: ".header-1 .letter",
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 4800 + 50 * i,
+});
+
+var textWrapper = document.querySelector(".header-2");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+    targets: ".header-2 .letter",
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 2000,
+    delay: (el, i) => 4800 + 50 * i,
+});
+
+var textWrapper = document.querySelector(".header-p");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+    targets: ".header-p .letter",
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1000,
+    delay: (el, i) => 3000 + 50 * i,
+});
+
+var textWrapper = document.querySelector(".button");
+textWrapper.innerHTML = textWrapper.textContent.replace(
+    /\S/g,
+    "<span class='letter'>$&</span>"
+);
+
+anime.timeline().add({
+    targets: ".button .letter",
+    translateY: [200, 0],
+    translateZ: 0,
+    opacity: [0, 1],
+    easing: "easeOutExpo",
+    duration: 1,
+    delay: (el, i) => 4800 + 50 * i,
+});
+
+TweenMax.to(".wrapper", 2, {
+    top: "-120%",
+    ease: Expo.easeInOut,
+    delay: 3.6,
+});
+
+var tl = new TimelineMax();
+
+tl.from(".loader", 1.6, {
+    scaleY: "0%",
+    y: 80,
+    ease: Expo.easeInOut,
+    delay: 1,
+    transformOrigin: "50% 100%",
+});
+
+tl.to(".loader", 1.6, {
+    height: "20vh",
+    scaleY: "0%",
+    ease: Expo.easeInOut,
+    transformOrigin: "0% -100%",
+});
+
+TweenMax.to(".box", 2.4, {
+    y: "-100%",
+    ease: Expo.easeInOut,
+    delay: 3.8,
+});
+
+var tl = new TweenMax.staggerFrom(
+    ".menu > div",
+    2,
+    {
+        opacity: 0,
+        y: 30,
+        ease: Expo.easeInOut,
+        delay: 4.2,
+    },
+    0.1
+);
+
+var tl = new TweenMax.staggerFrom(
+    ".hero-container > div",
+    2,
+    {
+        opacity: 0,
+        y: 30,
+        ease: Expo.easeInOut,
+        delay: 4.2,
+    },
+    0.1
+);
